a5ed4a21f0c950e5dac088b8bf6f7dad502e0acf
dmitriy0 <sevdmitriy0804@mail.ru>
2019-11-16 21:41:18 +0300

    fggf

---
+++ app/src/main/java/com/example/zoohack/ProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.zoohack;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
