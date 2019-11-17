fb5f8d4a52b5c91ad944f360833f5ee8d8c4a188
American Center Visitor <americancentervisitor@admins-imac-2.lan>
2019-11-17 15:39:37 +0300

    call

--- app/src/main/java/com/example/zoohack/DashboardFragment.java
+++ app/src/main/java/com/example/zoohack/DashboardFragment.java
@@ -1,14 +1,22 @@
 package com.example.zoohack;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -32,13 +40,13 @@ public class DashboardFragment extends Fragment {
         report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText1 = rootView.findViewById(R.id.type);
+                editText1 = rootView.findViewById(R.id.name);
                 type = editText1.getText().toString();
                 editText2 = rootView.findViewById(R.id.place);
                 place = editText2.getText().toString();
-                editText3 = rootView.findViewById(R.id.affected);
+                editText3 = rootView.findViewById(R.id.aff);
                 affected = editText3.getText().toString();
-                editText4 = rootView.findViewById(R.id.count);
+                editText4 = rootView.findViewById(R.id.num);
                 count = editText4.getText().toString();
                 myRef.child("problem").child("type").setValue(type); // Value
                 myRef.child("problem").child("place").setValue(place); // Value
@@ -46,6 +54,20 @@ public class DashboardFragment extends Fragment {
                 myRef.child("problem").child("count").setValue(count); // Value
             }
         });
+
+        Button call = rootView.findViewById(R.id.call); // authorisation
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "89670898899", null)));
+            }
+        });
+
         return rootView;
+
+
+
+
     }
 }
