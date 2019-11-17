2e698cbcd46e7114752a00565eb2cc50361a01ba
American Center Visitor <americancentervisitor@admins-imac-2.lan>
2019-11-17 16:56:28 +0300

    final

--- app/src/main/java/com/example/zoohack/DashboardFragment.java
+++ app/src/main/java/com/example/zoohack/DashboardFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
